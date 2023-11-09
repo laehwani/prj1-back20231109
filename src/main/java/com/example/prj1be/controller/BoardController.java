63a0557226dd055dbbc769c88bc84c33dbf92f2e
hwani <klh481@naver.com>
2023-11-09 14:18:00 +0900

    빈 값이 있으면 badRequest 응답함

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -1,7 +1,11 @@
 package com.example.prj1be.controller;
 
+import com.example.prj1be.domain.Board;
+import com.example.prj1be.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/board")
 public class BoardController {
 
+   private final BoardService service;
    @PostMapping("add")
-   public void add() {
-      System.out.println("BoardController.add");
+   public ResponseEntity<Object> add(@RequestBody Board board
+
+   ) {
+      if (!service.validate(board)) {
+         return ResponseEntity.badRequest().build();
+      }
+      if (service.save(board)) {
+         return ResponseEntity.ok().build();
+      }else {
+         return ResponseEntity.internalServerError().build();
+      }
    }
 
 }
---
+++ src/main/java/com/example/prj1be/domain/Board.java
@@ -0,0 +1,13 @@
+package com.example.prj1be.domain;
+
+import lombok.Data;
+
+@Data
+public class Board {
+
+   private String title;
+   private String content;
+   private String writer;
+
+
+}
