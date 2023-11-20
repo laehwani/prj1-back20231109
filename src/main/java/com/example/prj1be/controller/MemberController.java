6dfd553ee31b24e43d483f95dd1a2c681d33d3b7
hwani <klh481@naver.com>
2023-11-20 18:38:02 +0900

    back 컨트롤러 get /api/member/login : 세션의 'login' attribute 응답추가.

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -135,4 +135,9 @@ public class MemberController {
          session.invalidate();
       }
    }
+
+   @GetMapping("login")
+   public Member login(@SessionAttribute(value = "login", required = false) Member login) {
+      return login;
+   }
 }
