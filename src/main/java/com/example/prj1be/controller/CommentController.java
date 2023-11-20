020471ab73f6e9ed197c6f05343b2c760c3ab4d5
hwani <klh481@naver.com>
2023-11-20 22:25:21 +0900

    back 댓글 쓰기 기능과 테이블 추가. 테이블은 노션참조.

---
+++ src/main/java/com/example/prj1be/controller/CommentController.java
@@ -0,0 +1,40 @@
+package com.example.prj1be.controller;
+
+import com.example.prj1be.domain.Comment;
+import com.example.prj1be.domain.Member;
+import com.example.prj1be.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comment")
+public class CommentController {
+
+   private final CommentService service;
+
+   @PostMapping("add")
+   public ResponseEntity add(@RequestBody Comment comment,
+      @SessionAttribute(value = "login", required = false)
+      Member login) {
+
+      if (login == null) {
+         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+      }
+      if (service.validate(comment)) {
+         if (service.add(comment, login)) {
+            return ResponseEntity.ok().build();
+         } else {
+            return ResponseEntity.internalServerError().build();
+         }
+      } else {
+         return ResponseEntity.badRequest().build();
+      }
+   }
+}
---
+++ src/main/java/com/example/prj1be/domain/Comment.java
@@ -0,0 +1,14 @@
+package com.example.prj1be.domain;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class Comment {
+
+   private Integer id;
+   private Integer boardId;
+   private String memberId;
+   private String comment;
+   private LocalDateTime inserted;
+}
