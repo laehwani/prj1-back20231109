ee10d48266e9a5126f37ebef5f7f612166ed5edd
hwani <klh481@naver.com>
2023-11-21 17:36:25 +0900

    back 댓글 수정시 권한, 유효성 검사...

--- src/main/java/com/example/prj1be/service/CommentService.java
+++ src/main/java/com/example/prj1be/service/CommentService.java
@@ -52,4 +52,17 @@ public class CommentService {
       return mapper.update(comment) == 1;
 
    }
+
+   public boolean editValidate(Comment comment) {
+      if (comment == null) {
+         return false;
+      }
+      if (comment.getId() == null) {
+         return false;
+      }
+      if (comment.getComment() == null || comment.getComment().isBlank()) {
+         return false;
+      }
+      return true;
+   }
 }
