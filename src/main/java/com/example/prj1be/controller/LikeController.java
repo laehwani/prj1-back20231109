7bd3b286b5ed0bad460a5be6005f6de5430ecf79
hwani <klh481@naver.com>
2023-11-21 22:47:20 +0900

    back LikeController 좋아요 기능 추가중....

--- src/main/java/com/example/prj1be/controller/LikeController.java
+++ src/main/java/com/example/prj1be/controller/LikeController.java
@@ -1,12 +1,17 @@
 package com.example.prj1be.controller;
 
 import com.example.prj1be.domain.Like;
+import com.example.prj1be.domain.Member;
 import com.example.prj1be.service.LikeService;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RequestMapping("/api/like")
 @RestController
@@ -16,7 +21,14 @@ public class LikeController {
    private final LikeService service;
 
    @PostMapping
-   public void like(@RequestBody Like like) {
-      service.update(like);
+   public ResponseEntity like(
+      @RequestBody Like like,
+      @SessionAttribute(value = "login", required = false) Member login) {
+
+      if (login == null) {
+         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+      }
+      service.update(like, login);
+      return null;
    }
 }
