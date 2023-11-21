86eff911dc760408ac16757be734a574b1f09213
hwani <klh481@naver.com>
2023-11-21 22:07:06 +0900

    back boardLike 좋아요 기능추가 위해 boardLike 컨트롤러 생성...

---
+++ src/main/java/com/example/prj1be/service/LikeService.java
@@ -0,0 +1,23 @@
+package com.example.prj1be.service;
+
+import com.example.prj1be.domain.Like;
+import com.example.prj1be.mapper.LikeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+
+   private final LikeMapper mapper;
+
+   public void update(Like like) {
+      // TODO : 메모용- 좋아요 기능을 처음 좋아요 누를 때 : insert , 다시 누르면 : delete 되게 만들자
+
+      int count = 0;
+      if (mapper.delete(like) == 0) {
+         count = mapper.insert(like);
+      }
+
+   }
+}
