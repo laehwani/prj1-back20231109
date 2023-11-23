c51c5bc6aafba8b5ceec7e051081308dde4076e6
hwani <klh481@naver.com>
2023-11-23 14:30:09 +0900

    back board도메인 게시물 작성 시간 "..전" 으로 출력하는 getAgo 메소드 작성...

--- src/main/java/com/example/prj1be/domain/Board.java
+++ src/main/java/com/example/prj1be/domain/Board.java
@@ -1,7 +1,10 @@
 package com.example.prj1be.domain;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import lombok.Data;
 
 @Data
@@ -15,4 +18,28 @@ public class Board {
    private LocalDateTime inserted;
    private Integer commentCount;
    private Integer countLike;
+
+   public String getAgo() {
+      LocalDateTime now = LocalDateTime.now();
+
+      if (inserted.isBefore(now.minusYears(1))) {
+         Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+         return between.get(ChronoUnit.YEARS) + "년 전";
+      } else if (inserted.isBefore(now.minusMonths(1))) {
+         Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+         return between.get(ChronoUnit.MONTHS) + "달 전";
+      } else if (inserted.isBefore(now.minusDays(1))) {
+         Period between = Period.between(inserted.toLocalDate(), now.toLocalDate());
+         return between.get(ChronoUnit.DAYS) + "일 전";
+      } else if (inserted.isBefore(now.minusHours(1))) {
+         Duration between = Duration.between(inserted, now);
+         return (between.getSeconds() / 60 / 60) + "시간 전";
+      } else if (inserted.isBefore(now.minusMinutes(1))) {
+         Duration between = Duration.between(inserted, now);
+         return (between.getSeconds() / 60) + "분 전";
+      } else {
+         Duration between = Duration.between(inserted, now);
+         return between.getSeconds() + "초 전";
+      }
+   }
 }
